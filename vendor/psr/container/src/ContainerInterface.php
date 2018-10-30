2f6050a239307b6389d7a085bb37c51b8649a1be
ttnppedr <priest0204@gmail.com>
2018-10-30 16:09:46 +0800

    psr container ContainerInterface

--- vendor/psr/container/src/ContainerInterface.php
+++ vendor/psr/container/src/ContainerInterface.php
@@ -7,11 +7,13 @@ namespace Psr\Container;
 
 /**
  * Describes the interface of a container that exposes methods to read its entries.
+ * 描述容器的 interface ，可以露出讀取 entries 的方法
  */
 interface ContainerInterface
 {
     /**
      * Finds an entry of the container by its identifier and returns it.
+     * 用 id 找出 container 的入口並回傳
      *
      * @param string $id Identifier of the entry to look for.
      *
@@ -25,9 +27,13 @@ interface ContainerInterface
     /**
      * Returns true if the container can return an entry for the given identifier.
      * Returns false otherwise.
+     * 回傳 ture ，如果容器可以回傳此 id 的入口
+     * 否則回傳 false
      *
      * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
      * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     * has 回傳 true ，不代表 get 就不會丟 exception
+     * 只是代表 get 不會丟出'找不到的 exception'
      *
      * @param string $id Identifier of the entry to look for.
      *
