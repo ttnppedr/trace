ca4f46c2142b9f1e452fe25039d13a59043ce42c
ttnppedr <priest0204@gmail.com>
2018-10-31 13:45:43 +0800

    Illuminate Contracts Container Contaner

--- vendor/laravel/framework/src/Illuminate/Contracts/Container/Container.php
+++ vendor/laravel/framework/src/Illuminate/Contracts/Container/Container.php
@@ -9,6 +9,7 @@ interface Container extends ContainerInterface
 {
     /**
      * Determine if the given abstract type has been bound.
+     * 判斷 abstract type 是否已被綁定
      *
      * @param  string  $abstract
      * @return bool
@@ -17,6 +18,7 @@ interface Container extends ContainerInterface
 
     /**
      * Alias a type to a different name.
+     * 為 abstract 取一個 alias
      *
      * @param  string  $abstract
      * @param  string  $alias
@@ -26,6 +28,7 @@ interface Container extends ContainerInterface
 
     /**
      * Assign a set of tags to a given binding.
+     * 指定一個標籤給 abstract
      *
      * @param  array|string  $abstracts
      * @param  array|mixed   ...$tags
@@ -35,6 +38,7 @@ interface Container extends ContainerInterface
 
     /**
      * Resolve all of the bindings for a given tag.
+     * 解析此 tag 中所有的綁定
      *
      * @param  string  $tag
      * @return array
@@ -43,6 +47,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register a binding with the container.
+     * 註冊綁定
      *
      * @param  string  $abstract
      * @param  \Closure|string|null  $concrete
@@ -53,6 +58,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register a binding if it hasn't already been registered.
+     * 註冊尚未註冊的綁定
      *
      * @param  string  $abstract
      * @param  \Closure|string|null  $concrete
@@ -63,6 +69,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register a shared binding in the container.
+     * 註冊共享的綁定
      *
      * @param  string  $abstract
      * @param  \Closure|string|null  $concrete
@@ -72,6 +79,7 @@ interface Container extends ContainerInterface
 
     /**
      * "Extend" an abstract type in the container.
+     * 繼承 abstract type
      *
      * @param  string    $abstract
      * @param  \Closure  $closure
@@ -83,6 +91,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register an existing instance as shared in the container.
+     * 註冊已經存在的實例為共享
      *
      * @param  string  $abstract
      * @param  mixed   $instance
@@ -92,6 +101,7 @@ interface Container extends ContainerInterface
 
     /**
      * Define a contextual binding.
+     * 定義 contextual 綁定
      *
      * @param  string|array  $concrete
      * @return \Illuminate\Contracts\Container\ContextualBindingBuilder
@@ -100,6 +110,7 @@ interface Container extends ContainerInterface
 
     /**
      * Get a closure to resolve the given type from the container.
+     * 從容器取得 closure 去解析傳入的 type
      *
      * @param  string  $abstract
      * @return \Closure
@@ -108,6 +119,7 @@ interface Container extends ContainerInterface
 
     /**
      * Resolve the given type from the container.
+     * 從容器解析 type
      *
      * @param  string  $abstract
      * @param  array  $parameters
@@ -117,6 +129,7 @@ interface Container extends ContainerInterface
 
     /**
      * Call the given Closure / class@method and inject its dependencies.
+     * 呼叫 Closure 或 @mthod 的 class ，並注入其相依
      *
      * @param  callable|string  $callback
      * @param  array  $parameters
@@ -127,6 +140,7 @@ interface Container extends ContainerInterface
 
     /**
      * Determine if the given abstract type has been resolved.
+     * 判定傳入的 abstract type 是否已被解析
      *
      * @param  string $abstract
      * @return bool
@@ -135,6 +149,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register a new resolving callback.
+     * 註冊新的解析中的 callback
      *
      * @param  \Closure|string  $abstract
      * @param  \Closure|null  $callback
@@ -144,6 +159,7 @@ interface Container extends ContainerInterface
 
     /**
      * Register a new after resolving callback.
+     * 註冊新的解析後的 callback
      *
      * @param  \Closure|string  $abstract
      * @param  \Closure|null  $callback
